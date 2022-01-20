a338414bcd8aca68288836f5d584af14d4931e83
Joel Ansbro <joelmatthewansbro@gmail.com>
2022-01-20 22:15:15 +0000

    messy python script to play about with json data

--- python.js
+++ python.js
@@ -1,13 +1,20 @@
+// this connects to the python scraping script and also passes the JSON as it is currently saved down locally
+// I will want to pass the JSON from script3.py into a mongoDB database
+// which would allow me to pass the 
+
 const express = require('express')
 const {spawn} = require('child_process');
 const app = express()
 const port = 3001
+const fs = require('fs');
+const cron = require('node-cron')
+
 
 app.get('/', (req, res) => {
  
  var dataToSend;
  // spawn new child process to call the python script
- const python = spawn('python', ['script1.py']);
+ const python = spawn('python', ['script3.py']);
  // collect data from script
  python.stdout.on('data', function (data) {
   console.log('Pipe data from python script ...');
@@ -21,6 +28,12 @@ app.get('/', (req, res) => {
  });
  
 })
+
+app.get('/articles', (req, res) => {
+    var data = JSON.parse(fs.readFileSync('scraped_articles.json', 'utf8'))
+    res.send(data)
+})
+
 app.listen(port, () => console.log(`Example app listening on port 
 ${port}!`))
 
@@ -29,3 +42,5 @@ ${port}!`))
 // The above code, sets up a basic express app with one get router. Inside the get router, we spawn a new child_process with parameters:
 
 // test out and try to implement https://github.com/codelucas/newspaper
+
+// https://medium.com/swlh/run-python-script-from-node-js-and-send-data-to-browser-15677fcf199f
